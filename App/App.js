a4cde392b2918ae096b843a13e060069830d448e
Alex Newman <Alex.newman@bedegaming.com>
2015-05-18 23:23:19 +0100

    added XML to JSON parser

--- App/App.js
+++ App/App.js
@@ -4,4 +4,15 @@ var $ = require('jquery'),
 	_ = require('underscore');
 
 $(function() {
+	$('button').on('click', function() {
+		myCodeMirror.save();
+
+		var recipe = $('#recipe').val();
+		recipe = xmlToJSON.parseString(recipe, {
+			childrenAsArray: false
+		}).Orchard.Recipe;
+
+
+		console.log(recipe)
+	});
 });
